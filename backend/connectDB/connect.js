b751a72a17e2e78c438257d62d9e29c2cb1863a1
digitalvysh <vyshnavibandla@gmail.com>
2022-06-09 20:33:08 +0530

    done

---
+++ backend/connectDB/connect.js
@@ -0,0 +1,6 @@
+const mongoose = require("mongoose");
+
+const main = async() => {
+    await mongoose.connect(process.env.DB);
+};
+module.exports = main;
---
+++ frontend/src/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import { StateProvider } from "./context/StateProvider";
+import initialState from "./context/initialState";
+import reducer from "./context/reducer";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <BrowserRouter>
+    <StateProvider initialState={initialState} reducer={reducer}>
+      <App />
+    </StateProvider>
+  </BrowserRouter>
+);
